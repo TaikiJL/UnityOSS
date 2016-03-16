301d8be918ecb15640cd862da81ae163da720784
TaikiJL <taiki.hagiwara@gmail.com>
2016-03-16 21:52:15 +0100

    Prefab preorder

---
+++ Assets/PrefabOrderer/Example/PrefabCustomer.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PrefabCustomer : MonoBehaviour
+{
+
+    public PrefabPreOrder order;
+
+    public GameObject reference;
+
+	void Start () {
+        order.Instantiate();
+
+        reference = order.instance as GameObject;
+	}
+
+}
---
+++ Assets/PrefabOrderer/PrefabPreOrder.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+[System.Serializable]
+public class PrefabPreOrder
+{
+
+    [SerializeField]
+    private Object m_Prefab;
+
+    public Object instance { get; private set; }
+
+    public void Instantiate()
+    {
+        instance = Object.Instantiate(m_Prefab);
+    }
+
+    public void Instantiate(Vector3 position, Quaternion rotation)
+    {
+        instance = Object.Instantiate(m_Prefab, position, rotation);
+    }
+
+}
