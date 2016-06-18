8057171bd757b10aa0736d338ce374d8576cd2c2
Taiki Hagiwara <thagiwarapro@gmail.com>
2016-06-18 11:49:54 +0200

    String function to count Unity Rich Texts without tags

---
+++ Assets/String/Stringf.cs
@@ -0,0 +1,34 @@
+﻿public struct Stringf
+{
+
+	/// <summary>
+	/// Returns the length of the given string without counting Rich Text tags.
+	/// </summary>
+	/// <returns>The text length.</returns>
+	/// <param name="str">String.</param>
+	public static int RichTextLength(string str)
+	{
+		if (string.IsNullOrEmpty (str))
+			return 0;
+
+		int length = 0;
+		int i = 0;
+
+		while (i < str.Length)
+		{
+			while (i < str.Length && !str[i].Equals('<'))
+			{
+				++i;
+				++length;
+			}
+			++i;
+
+			while (i < str.Length && !str[i].Equals('>'))
+				++i;
+			++i;
+		}
+
+		return length;
+	}
+
+}
