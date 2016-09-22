2a09c0b7669309f343f9ebb3aac6d8b7510c8030
Taiki Hagiwara <thagiwarapro@gmail.com>
2016-09-22 20:48:07 +0200

    project settings test

---
+++ Assets/PSTest.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PSTest : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnGUI()
+    {
+        GUI.color = TestProjectSettings.SomeColor;
+        GUILayout.Label("Baboulinet");
+    }
+
+}
