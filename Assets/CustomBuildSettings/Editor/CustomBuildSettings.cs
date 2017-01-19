fe9638701a5cc460eb7d31f7433577620ae8be6b
Taiki Hagiwara <thagiwarapro@gmail.com>
2017-01-19 23:38:26 +0100

    started custombuildsettings

---
+++ Assets/CustomBuildSettings/Editor/CustomBuildSettings.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using System.IO;
+
+public class CustomBuildSettings : ScriptableObject
+{
+
+	[SerializeField]
+	private string[] m_CustomSymbols;
+
+	public static CustomBuildSettings Load()
+	{
+		string path = "Assets/EditorDefaultResources/CustomBuildSettings.asset";
+		CustomBuildSettings settings;
+		if (!File.Exists(path))
+		{
+			settings = CreateInstance<CustomBuildSettings>();
+			AssetDatabase.CreateAsset(settings, path);
+		}
+		else
+		{
+			settings = AssetDatabase.LoadAssetAtPath<CustomBuildSettings>(path);
+		}
+
+		return settings;
+	}
+
+	public string[] symbols { get { return m_CustomSymbols; } }
+
+}
---
+++ Assets/CustomBuildSettings/Editor/CustomBuildWindow.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class CustomBuildWindow : ScriptableWizard
+{
+
+	private CustomBuildSettings m_Settings = null;
+
+    [MenuItem("Build/Window")]
+    static void CreateWizard()
+    {
+        var window = ScriptableWizard.DisplayWizard<CustomBuildWindow>("Custom Build Settings", "Build");
+		if (window.m_Settings == null)
+			window.m_Settings = CustomBuildSettings.Load();
+    }
+
+    void OnWizardCreate()
+    {
+		
+    }
+
+    void OnWizardUpdate()
+    {
+        helpString = "Please set the color of the light!";
+    }
+
+}
